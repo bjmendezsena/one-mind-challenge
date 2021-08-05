0825dec43f56369ab23f501369e5c9aee885069f
Briaulin Mendez <b.j.mendezsena@gmail.com>
2021-08-06 00:54:04 +0200

    first commit

---
+++ src/MainApp.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+
+import { Button } from "antd";
+
+import "./MainApp.css";
+
+const MainApp: FC = () => {
+  return (
+    <div>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
+
+export default MainApp;
